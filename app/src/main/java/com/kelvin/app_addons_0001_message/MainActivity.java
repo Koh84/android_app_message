84b2ace778200b8244861ade08f426dc60b89d93
kelvinkoh <koh84@hotmail.com>
2017-12-08 02:56:40 +0000

    v3, support Message with looper and handler

--- app/src/main/java/com/kelvin/app_addons_0001_message/MainActivity.java
+++ app/src/main/java/com/kelvin/app_addons_0001_message/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kelvin.app_addons_0001_message;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private int ButtonCount = 0;
     private Thread myThread;
     private MyThread myThread2;
+    private Handler mHandler;
+    private int nMessageCount = 0;
 
     class myRunnable implements Runnable{
         @Override
@@ -38,20 +43,36 @@ public class MainActivity extends AppCompatActivity {
 
     //The above way of implementing thread won't allow more than 1 function
     //Hence we use this method below
+    //This thread service any incoming message
     class MyThread extends Thread{
+        private Looper mLooper;
         @Override
         public void run() {
             super.run();
-            int count = 0;
-            for(;;){
-                Log.d(TAG, "MyThread2 "+count);
-                count++;
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+            Looper.prepare();   //This prepare a queue
+            synchronized (this) {
+                mLooper = Looper.myLooper();
+                notifyAll();
+            }
+            Looper.loop();      //This service the message
+        }
+
+        public Looper getLooper() {
+            if (!isAlive()) {
+                return null;
+            }
+
+            // If the thread has been started, wait until the looper has been created.
+            synchronized (this) {
+                while (isAlive() && mLooper == null) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                    }
                 }
             }
+            return mLooper;
         }
     }
 
@@ -66,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                 // Code here executes on main thread after user presses button
                 Log.d(TAG, "Send Message "+ButtonCount);
                 ButtonCount++;
+                Message msg = new Message();
+                mHandler.sendMessage(msg);
             }
         });
 
@@ -74,6 +97,19 @@ public class MainActivity extends AppCompatActivity {
 
         myThread2 = new MyThread();
         myThread2.start();
+
+        //Handler is used to send message
+        //Below link the Handler to the looper and when the
+        //looper received the messsage, it will use the callback function
+        //handleMessage
+        mHandler = new Handler(myThread2.getLooper(), new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Log.d(TAG, "get Message "+nMessageCount);
+                nMessageCount++;
+                return false;
+            }
+        });
     }
 
     @Override
